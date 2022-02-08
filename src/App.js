2e1833761c2328dd8dbb6819d76588eae2e545c6
Heze <64218603+Hcmwebs@users.noreply.github.com>
2022-02-08 21:34:54 +1300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+const logo = 'headers.ltd';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<h3>{logo}</h3>
+				<nav className='nav'>
+          <ul className="navbar">
+            <li>about</li>
+            <li>pricing</li>
+            <li>contact</li>
+          </ul>
+				</nav>
+        <div className="hero">
+          <img src="" alt="" className="img" />
+        </div>
+			</header>
+		</div>
+	);
 }
 
 export default App;
